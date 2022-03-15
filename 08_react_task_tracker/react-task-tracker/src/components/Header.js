2ae7ddd8ceb03b112719d9a031298b4e8074a05f
Toh Yan Jie <yanjie20009@gmail.com>
2022-03-15 23:02:56 +0800

    Add Task Tracker

---
+++ 08_react_task_tracker/react-task-tracker/src/components/Header.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+
+export const Header = (props) => {
+  return (
+    <header>
+        <h1>
+            {props.title}
+        </h1>
+    </header>
+
+  )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+export default Header
